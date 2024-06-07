b27fbc605c993a1e568116ac9e78b925ba7aaaa8
Tyler <tylerjstocks@gmail.com>
2024-06-06 22:41:16 -0700

    Began implementation of constants property parser. Still needs a little bit of cleanup and some further testing, but it seems to be working well enough for now.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.annotation.SuppressLint;
+
 import androidx.annotation.NonNull;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -7,12 +9,40 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 
+/**
+ * <h1>Constants Class</h1>
+ *
+ * <h2>Subsystem Nested Classes</h2>
+ * <p>
+ *     Each subsystem declared in the Subsystems package has a corresponding nested class that
+ *     contains the related values. Due to the way that the constants loader is implemented, any
+ *     constants values that are outside of a nested class will be ignored. This can be useful if
+ *     you would like to have a constant that cannot be overwritten from onbot java such as the
+ *     config file name of a hardware device.
+ * </p>
+ * <br>
+ * <h2>Constants Loader</h2>
+ * <p>
+ *     The constants loader is a class that loads a properties file from Constants.txt found in
+ *     onbot java and fills in all of the fields for all of constants nested classes using
+ *     reflection. Due to this approach there is no need to modify this class if more constants, or
+ *     nested classes are added.
+ * </p>
+ * <br>
+ * <p>
+ *     Another important functionality of this class is to all the saving of all constants to the
+ *     aforementioned properties file in onbot java. By calling the saveConstantsFile() function you
+ *     can overwrite all of the values in onbot java with the ones that are hardcoded into the
+ *     class. This operation is very dangerous as once the values are overwritten there is no way to
+ *     get them back unless you have a backup.
+ * </p>
+ */
 public class Constants {
 
     public static class LauncherConstants {
@@ -61,40 +91,70 @@ public class Constants {
         public static volatile double OUTTAKE_DOOR_CLOSED_POSITION = 0.0;
     }
 
+    /**
+     * <h1>Constants Loader</h1>
+     * <p>
+     *     The constants loader is a class that loads a properties file from Constants.txt found in
+     *     onbot java and fills in all of the fields for all of constants nested classes using
+     *     reflection. Due to this approach there is no need to modify this class if more constants, or
+     *     nested classes are added.
+     * </p>
+     * <br>
+     * <p>
+     *     Another important functionality of this class is to all the saving of all constants to the
+     *     aforementioned properties file in onbot java. By calling the saveConstantsFile() function you
+     *     can overwrite all of the values in onbot java with the ones that are hardcoded into the
+     *     class. This operation is very dangerous as once the values are overwritten there is no way to
+     *     get them back unless you have a backup.
+     * </p>
+     */
     public static class ConstantsLoader {
        private static final Properties properties = new Properties();
 
+       /*
+       TODO Try out Environment.getExternalStorageDirectory() so that it stops bugging me and we can
+       TODO rid of this stupid SuppressLint
+        */
+       @SuppressLint("SdCardPath")
        private static final String CONSTANTS_FILE_LOCATION  = "/sdcard/FIRST/java/src/org/firstinspires/ftc/teamcode/";
        private static final String CONSTANTS_FILE_NAME      = "Constants.txt";
 
        private static Telemetry telemetry;
 
-       public static void load(@NonNull Telemetry telemetry) {
-           setTelemetry(telemetry);
+        /**
+         * Loads constants from a properties file located in onbot java.
+         * @param telemetry The telemetry to display errors on
+         */
+       public static void load(@NonNull Telemetry telemetry) throws IOException {
+           ConstantsLoader.telemetry = telemetry;
+
            loadPropertiesFile();
            populateConstantsClass();
+
+           telemetry.update();
        }
 
-       public static void setTelemetry(@NonNull Telemetry telemetry) {
-           ConstantsLoader.telemetry = telemetry;
-       }
-
-       private static void loadPropertiesFile() {
-            try {
+        /**
+         * Loads the properties file
+         * @throws IOException Throws an IOException if the properties file cannot be loaded
+         */
+       private static void loadPropertiesFile() throws IOException {
                 properties.clear();
                 properties.load(new FileInputStream(CONSTANTS_FILE_LOCATION + CONSTANTS_FILE_NAME));
-            } catch (Exception exception) {
-                telemetry.addData("Exception in loading properties", exception.getMessage());
-           }
        }
 
+        /**
+         * Populates a field with its value from the properties file. If a key matching the name of
+         * the field is not found, the function will simply do nothing.
+         * @param field The field to populate
+         */
        private static void populateField(@NonNull Field field) {
            String fieldName   = field.getName();
            Class<?> fieldType = field.getType();
 
-           try {
-               if (!properties.containsKey(fieldName)) return;
+           if (!properties.containsKey(fieldName)) return;
 
+           try {
               if (fieldType.isAssignableFrom(double.class)) {
                   field.setDouble(fieldName, getDoubleFromPropertiesFile(fieldName));
               }
@@ -102,11 +162,16 @@ public class Constants {
               if (fieldType.isAssignableFrom(int.class)) {
                   field.setInt(fieldName, getIntFromPropertiesFile(fieldName));
               }
-          } catch (Exception exception) {
-              telemetry.addData("Exception in populating field", exception.getMessage());
+          } catch (IllegalAccessException exception) {
+                telemetry.addData("Illegal access to field", fieldName);
            }
        }
 
+        /**
+         * Populates all of the values of one of the nested classes with the matching value found
+         * in Constants.txt in onbot java
+         * @param clazz The class to populate the fields of
+         */
        private static void populateClass(@NonNull Class<?> clazz) {
            Field[] fields = clazz.getDeclaredFields();
 
@@ -117,6 +182,10 @@ public class Constants {
            }
        }
 
+        /**
+         * Populates the constants class with values from the Constants.txt file located in onbot
+         * java
+         */
        private static void populateConstantsClass() {
            Class<?>[] classes = Constants.class.getDeclaredClasses();
 
@@ -128,11 +197,61 @@ public class Constants {
             }
        }
 
-       private static void saveFieldToConstantsFile(@NonNull Field field) {
+        /**
+         * Saves a field to the constants file
+         * @param field The field to save to the constants file
+         * @throws IOException Throws and IOException if the file cannot be written to
+         */
+       private static void saveFieldToConstantsFile(@NonNull Field field) throws IOException {
+           String fieldName   = field.getName();
+           Class<?> fieldType = field.getType();
+
+           String output = "";
+
            try {
-               FileWriter writer = new FileWriter(CONSTANTS_FILE_LOCATION + CONSTANTS_FILE_NAME);
-           } catch (Exception exception) {
-               telemetry.addData("Exception in saving field to constants file", exception.getMessage());
+               if (fieldType.isAssignableFrom(double.class)) {
+                   output = fieldName + "=" + field.getDouble(null);
+               }
+
+               if (fieldType.isAssignableFrom(int.class)) {
+                   output = fieldName + "=" + field.getInt(null);
+               }
+
+           } catch (IllegalAccessException exception) {
+                telemetry.addData("Illegal access to field", fieldName);
+           }
+
+           if (output.isEmpty()) return;
+
+           Writer writer = new FileWriter(CONSTANTS_FILE_LOCATION + CONSTANTS_FILE_NAME, true);
+           BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+           bufferedWriter.write(output);
+           bufferedWriter.close();
+       }
+
+        /**
+         * Saves all of the fields in one nested class to the properties file
+         * @param clazz The nested class to save the fields of
+         * @throws IOException Throws an IOException if the properties file cannot be written to
+         */
+       private static void saveNestedClassToConstantsFile(@NonNull Class<?> clazz) throws IOException {
+           Field[] fields = clazz.getDeclaredFields();
+
+           for (Field field : fields) {
+               saveFieldToConstantsFile(field);
+           }
+       }
+
+        /**
+         * Saves all constants in the Constants class to a file in onbot java (Constants.txt)
+         * @throws IOException Throws an IOException if Constants.txt cannot be written to.
+         */
+       public static void saveConstantsToFile() throws IOException {
+           Class<?>[] classes = Constants.class.getDeclaredClasses();
+
+           for (Class<?> clazz : classes) {
+               saveNestedClassToConstantsFile(clazz);
            }
        }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Utility/PopulatePropertiesFile.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.OpModes.Utility;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import static org.firstinspires.ftc.teamcode.Constants.ConstantsLoader;
+
+import java.io.IOException;
+
+@TeleOp(name = "Utility - Populate Properties File", group = "Utility")
+@Disabled
+public class PopulatePropertiesFile extends LinearOpMode {
+
+   @Override public void runOpMode() {
+        telemetry.addLine("Warning! On start this opmode will overwrite all values");
+        telemetry.addLine("stored in constants file in onbot java (Constants.txt) with");
+        telemetry.addLine("the values in Constants.java located in Android Studio. DO");
+        telemetry.addLine("NOT run this opmode unless you are 120% certain you know");
+        telemetry.addLine("what you are doing as there is NO WAY TO UNDO IT.");
+        telemetry.update();
+
+        waitForStart();
+
+        try {
+            ConstantsLoader.saveConstantsToFile();
+            telemetry.addLine("Successfully overwrote Constants.txt");
+        } catch (IOException exception) {
+            telemetry.addData("Failed to overwrite Constants.txt", exception.getMessage());
+        }
+
+        requestOpModeStop();
+   }
+
+}
