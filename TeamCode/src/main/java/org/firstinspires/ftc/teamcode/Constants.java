8f8b63d550f58f6d4656de3e2f92f78bcf62a428
Tyler <tylerjstocks@gmail.com>
2024-06-07 22:09:08 -0700

    Began to implement a properties file parser that works with multiple files and nested classes dynamically.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -2,18 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 import android.annotation.SuppressLint;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.acmerobotics.dashboard.config.Config;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Properties;
 
 /**
@@ -117,160 +116,89 @@ public class Constants {
      * </p>
      */
     public static class ConstantsLoader {
-       private static final Properties properties = new Properties();
+        private static ArrayList<Properties> propertiesFiles = new ArrayList<>();
 
         /*
-         * Environment.getExternalStorageDirectory() doesn't actually work so we suppress it with a
-         * lint.
+         Environment.getExternalStorageDirectory() doesn't actually work so we suppress it with a
+         lint.
          */
        @SuppressLint("SdCardPath")
        private static final String CONSTANTS_FILE_LOCATION
-               = "/sdcard/FIRST/java/src/org/firstinspires/ftc/teamcode/";
-       private static final String CONSTANTS_FILE_NAME
-               = "Constants.txt";
+               = "/sdcard/FIRST/java/src/org/firstinspires/ftc/teamcode/Constants/";
 
        private static Telemetry telemetry;
 
-        /**
-         * Loads constants from a properties file located in onbot java.
-         * @param telemetry The telemetry to display errors on
-         */
-       public static void load(@NonNull Telemetry telemetry) throws IOException {
-           ConstantsLoader.telemetry = telemetry;
-
-           loadPropertiesFile();
-           populateConstantsClass();
-
-           telemetry.update();
+       public static void setTelemetry(@NonNull Telemetry telemetry) {
+          ConstantsLoader.telemetry = telemetry;
        }
 
-        /**
-         * Loads the properties file
-         * @throws IOException Throws an IOException if the properties file cannot be loaded
-         */
-       private static void loadPropertiesFile() throws IOException {
-                properties.clear();
-                properties.load(new FileInputStream(CONSTANTS_FILE_LOCATION + CONSTANTS_FILE_NAME));
+       private static File[] getConstantsFiles() throws IOException{
+          File constantsDirectory = new File(CONSTANTS_FILE_LOCATION);
+
+          if (constantsDirectory.isFile()) throw new IOException("Constants directory is a file");
+
+          return constantsDirectory.listFiles();
        }
 
-        /**
-         * Populates a field with its value from the properties file. If a key matching the name of
-         * the field is not found, the function will simply do nothing.
-         * @param field The field to populate
-         */
-       private static void populateField(@NonNull Field field) {
-           String fieldName   = field.getName();
-           Class<?> fieldType = field.getType();
+       @NonNull private static String parseFileName(@NonNull String fileName) {
+           String[] wordsInFileName = fileName.split("(?<!^)(?=[A-Z])");
 
-           if (!properties.containsKey(fieldName)) return;
+           String lastWordInFileName = wordsInFileName[wordsInFileName.length - 1];
 
-           try {
-              if (fieldType.isAssignableFrom(double.class)) {
-                  field.setDouble(fieldName, getDoubleFromPropertiesFile(fieldName));
-              }
+           if (!lastWordInFileName.equals("Constants")) return "";
 
-              if (fieldType.isAssignableFrom(int.class)) {
-                  field.setInt(fieldName, getIntFromPropertiesFile(fieldName));
-              }
-          } catch (IllegalAccessException exception) {
-                telemetry.addData("Illegal access to field", fieldName);
-           }
+           return wordsInFileName[0];
        }
 
-        /**
-         * Populates all of the values of one of the nested classes with the matching value found
-         * in Constants.txt in onbot java
-         * @param clazz The class to populate the fields of
-         */
-       private static void populateClass(@NonNull Class<?> clazz) {
+       @Nullable private static Class<?> matchConstantsClassToConstantsFile(@NonNull String fileIdentifier) {
+          for (Class<?> clazz : ConstantsLoader.class.getDeclaredClasses()) {
+              // Skip the constants loader
+              if (clazz.getName().equals("ConstantsLoaderClass")) continue;
+
+              if (clazz.getName().equals(fileIdentifier)) return clazz;
+          }
+
+          return null;
+       }
+
+       public static void populateClassFromPropertiesFile(@NonNull Class<?> clazz, @NonNull String fileName) throws IOException {
+           Properties properties = new Properties();
+           properties.load(new FileInputStream(CONSTANTS_FILE_LOCATION + fileName));
+
            Field[] fields = clazz.getDeclaredFields();
 
            for (Field field : fields) {
-               if (!Modifier.isStatic(field.getModifiers())) continue;
 
-               populateField(field);
            }
        }
 
-        /**
-         * Populates the constants class with values from the Constants.txt file located in onbot
-         * java
-         */
-       private static void populateConstantsClass() {
-           Class<?>[] classes = Constants.class.getDeclaredClasses();
 
-           for (Class<?> clazz : classes) {
-                // Skip the constants loader class
-                if (clazz.getName().equals("ConstantsLoader")) continue;
 
-                populateClass(clazz);
+       public static void loadConstants() throws IOException {
+            File[] constantsFiles = getConstantsFiles();
+
+            for (File file : constantsFiles) {
+                String fileName = file.getName();
+
+                String fileIdentifier = parseFileName(fileName);
+
+                // Skip the file if it does not end in "Constants"
+                if (fileIdentifier.isEmpty()) continue;
+
+                Class<?> constantsClass = matchConstantsClassToConstantsFile(fileIdentifier);
+
+                // If there is no constants file that matches a constants class, continue.
+                if (constantsClass == null) continue;
+
+
+
             }
        }
 
-        /**
-         * Saves a field to the constants file
-         * @param field The field to save to the constants file
-         * @throws IOException Throws and IOException if the file cannot be written to
-         */
-       private static void saveFieldToConstantsFile(@NonNull Field field) throws IOException {
-           String fieldName   = field.getName();
-           Class<?> fieldType = field.getType();
 
-           String output = "";
 
-           try {
-               if (fieldType.isAssignableFrom(double.class)) {
-                   output = fieldName + "=" + field.getDouble(null);
-               }
 
-               if (fieldType.isAssignableFrom(int.class)) {
-                   output = fieldName + "=" + field.getInt(null);
-               }
 
-           } catch (IllegalAccessException exception) {
-                telemetry.addData("Illegal access to field", fieldName);
-           }
 
-           if (output.isEmpty()) return;
-
-           Writer writer = new FileWriter(CONSTANTS_FILE_LOCATION + CONSTANTS_FILE_NAME, true);
-           BufferedWriter bufferedWriter = new BufferedWriter(writer);
-
-           bufferedWriter.write(output);
-           bufferedWriter.close();
-       }
-
-        /**
-         * Saves all of the fields in one nested class to the properties file
-         * @param clazz The nested class to save the fields of
-         * @throws IOException Throws an IOException if the properties file cannot be written to
-         */
-       private static void saveNestedClassToConstantsFile(@NonNull Class<?> clazz) throws IOException {
-           Field[] fields = clazz.getDeclaredFields();
-
-           for (Field field : fields) {
-               saveFieldToConstantsFile(field);
-           }
-       }
-
-        /**
-         * Saves all constants in the Constants class to a file in onbot java (Constants.txt)
-         * @throws IOException Throws an IOException if Constants.txt cannot be written to.
-         */
-       public static void saveConstantsToFile() throws IOException {
-           Class<?>[] classes = Constants.class.getDeclaredClasses();
-
-           for (Class<?> clazz : classes) {
-               saveNestedClassToConstantsFile(clazz);
-           }
-       }
-
-        private static double getDoubleFromPropertiesFile(@NonNull String key) {
-            return Double.parseDouble(properties.getProperty(key));
-        }
-
-        private static int getIntFromPropertiesFile(@NonNull String key) {
-            return Integer.parseInt(properties.getProperty(key));
-        }
     }
 }
